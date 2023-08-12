aa8661dd5f55d6a6e07207466e9e1117ed583ffd
KAN NINOMIYA <ninomiya@go5.run>
2023-08-13 03:29:42 +0900

    cover

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,14 +12,21 @@ export default async function Home() {
             'last_edited_time' in post &&
             'properties' in post &&
             'Name' in post.properties &&
-            'title' in post.properties.Name
+            'title' in post.properties.Name &&
+            'id' in post
           ) {
             const date = new Date(post.last_edited_time).toLocaleString()
             const titleText = post.properties.Name.title
               .map((textItem) => textItem.plain_text)
               .join('')
+            let coverUrl = ''
+            if (post.cover?.type === 'external') {
+              coverUrl = post.cover.external.url
+            }
+
             return (
               <li key={post.id}>
+                <img src={coverUrl} />
                 <h3>
                   <Link href={`/${post.id}`}>
                     <p>{titleText}</p>
