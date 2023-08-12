b27391c9924b05ae9c02f2b88047d43eef0b87ea
KAN NINOMIYA <ninomiya@go5.run>
2023-08-13 02:18:22 +0900

    天下のIntelliJによる型

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,28 +1,81 @@
 import {getBlocks, getPage} from "@/lib/notion";
-import {Fragment} from "react";
+import {createElement, Fragment, Key} from "react";
 import Link from "next/link";
 
-const Text = ({text}) => {
-    if (!text) {
-        return null;
+export default async function Post({params}: {
+    params: {
+        id: string
     }
-    return text.map((value) => {
-        const {
-            annotations: {bold, code, color, italic, strikethrough, underline},
-            text,
-        } = value;
-        return (
-            <span
-                style={color !== "default" ? {color} : {}}
-                key={text.content}
-            >
-        {text.link ? <a href={text.link.url}>{text.content}</a> : text.content}
-      </span>
-        );
-    });
+}) {
+    const page = await getPage(params.id);
+    const blocks = await getBlocks(params.id);
+    if (!page || !blocks) {
+        return <div/>;
+    }
+    return (
+        <div>
+            <article>
+                <h1>
+                    <>
+                        {"properties" in page && "Name" in page.properties && "title" in page.properties.Name
+                            ? (
+                                <p>{page.properties.Name.title.map(textItem => textItem.plain_text).join('')}</p>
+                            )
+                            : "データが不足しています。"
+                        }
+                    </>
+                </h1>
+                <section>
+                    {blocks.map((block: {
+                        [x: string]: any;
+                        id: any;
+                        children?: any;
+                        type?: any;
+                    }) => (
+                        <Fragment key={block.id}>{renderBlock(block)}</Fragment>
+                    ))}
+                    <Link href="/">
+                        ← Go home
+                    </Link>
+                </section>
+            </article>
+        </div>
+    );
+}
+
+
+type TextType = {
+    annotations: {
+        color: string;
+    };
+    content: string;
+    link?: {
+        url: string;
+    };
+}[];
+
+
+const Text = ({text}: {
+    text: TextType
+}) => {
+    if (!text) return null;
+
+    return text.map(({annotations: {color}, content, link}) => (
+        <span style={color !== "default" ? {color} : {}} key={content}>
+            {link ? <a href={link.url}>{content}</a> : content}
+        </span>
+    ));
 };
 
-const renderNestedList = (block) => {
+const renderHeader = (text: {
+    rich_text: TextType;
+}, tag: string) => createElement(tag, null, <Text
+    text={text.rich_text}/>);
+
+const renderNestedList = (block: {
+    [x: string]: any;
+    type?: any;
+}) => {
     const {type} = block;
     const value = block[type];
     if (!value) return null;
@@ -30,22 +83,23 @@ const renderNestedList = (block) => {
     const isNumberedList = value.children[0].type === "numbered_list_item";
 
     if (isNumberedList) {
-        return <ol>{value.children.map((block) => renderBlock(block))}</ol>;
+        return <ol>{value.children.map((block: any) => renderBlock(block))}</ol>;
     }
-    return <ul>{value.children.map((block) => renderBlock(block))}</ul>;
+    return <ul>{value.children.map((block: any) => renderBlock(block))}</ul>;
 };
 
-const renderBlock = (block) => {
+const renderBlock = (block: {
+    [x: string]: any;
+    id?: any;
+    children?: any;
+    type?: any;
+}) => {
     const {type, id} = block;
     const value = block[type];
 
     switch (type) {
         case "paragraph":
-            return (
-                <p>
-                    <Text text={value.rich_text}/>
-                </p>
-            );
+            return <p><Text text={value.rich_text}/></p>;
         case "heading_1":
             return (
                 <h1>
@@ -59,16 +113,22 @@ const renderBlock = (block) => {
                 </h2>
             );
         case "heading_3":
-            return (
-                <h3>
-                    <Text text={value.rich_text}/>
-                </h3>
-            );
+            return renderHeader(value, type.replace('_', ''));
         case "bulleted_list": {
-            return <ul>{value.children.map((child) => renderBlock(child))}</ul>;
+            return <ul>{value.children.map((child: {
+                [x: string]: any;
+                id?: any;
+                children?: any;
+                type?: any;
+            }) => renderBlock(child))}</ul>;
         }
         case "numbered_list": {
-            return <ol>{value.children.map((child) => renderBlock(child))}</ol>;
+            return <ol>{value.children.map((child: {
+                [x: string]: any;
+                id?: any;
+                children?: any;
+                type?: any;
+            }) => renderBlock(child))}</ol>;
         }
         case "bulleted_list_item":
         case "numbered_list_item":
@@ -93,7 +153,12 @@ const renderBlock = (block) => {
                     <summary>
                         <Text text={value.rich_text}/>
                     </summary>
-                    {block.children?.map((child) => (
+                    {block.children?.map((child: {
+                        [x: string]: any;
+                        id: any;
+                        children?: any;
+                        type?: any;
+                    }) => (
                         <Fragment key={child.id}>{renderBlock(child)}</Fragment>
                     ))}
                 </details>
@@ -102,18 +167,13 @@ const renderBlock = (block) => {
             return (
                 <div>
                     <strong>{value.title}</strong>
-                    {block.children.map((child) => renderBlock(child))}
+                    {block.children.map((child: {
+                        [x: string]: any;
+                        id?: any;
+                        children?: any;
+                        type?: any;
+                    }) => renderBlock(child))}
                 </div>
-            );
-        case "image":
-            const src =
-                value.type === "external" ? value.external.url : value.file.url;
-            const caption = value.caption ? value.caption[0]?.plain_text : "";
-            return (
-                <figure>
-                    <img src={src} alt={caption}/>
-                    {caption && <figcaption>{caption}</figcaption>}
-                </figure>
             );
         case "divider":
             return <hr key={id}/>;
@@ -126,6 +186,16 @@ const renderBlock = (block) => {
             {value.rich_text[0].plain_text}
           </code>
         </pre>
+            );
+        case "image":
+            const src =
+                value.type === "external" ? value.external.url : value.file.url;
+            const caption = value.caption ? value.caption[0]?.plain_text : "";
+            return (
+                <figure>
+                    <img src={src} alt={caption}/>
+                    {caption && <figcaption>{caption}</figcaption>}
+                </figure>
             );
         case "file":
             const src_file =
@@ -155,7 +225,12 @@ const renderBlock = (block) => {
             return (
                 <table>
                     <tbody>
-                    {block.children?.map((child, i) => {
+                    {block.children?.map((child: {
+                        id: Key | null | undefined;
+                        table_row: {
+                            cells: any[];
+                        };
+                    }, i: number) => {
                         const RowElement =
                             value.has_column_header && i == 0 ? "th" : "td";
                         return (
@@ -177,42 +252,24 @@ const renderBlock = (block) => {
         case "column_list": {
             return (
                 <div>
-                    {block.children.map((block) => renderBlock(block))}
+                    {block.children.map((block: {
+                        [x: string]: any;
+                        id?: any;
+                        children?: any;
+                        type?: any;
+                    }) => renderBlock(block))}
                 </div>
             );
         }
         case "column": {
-            return <div>{block.children.map((child) => renderBlock(child))}</div>;
+            return <div>{block.children.map((child: {
+                [x: string]: any;
+                id?: any;
+                children?: any;
+                type?: any;
+            }) => renderBlock(child))}</div>;
         }
         default:
-            return `❌ Unsupported block (${
-                type === "unsupported" ? "unsupported by Notion API" : type
-            })`;
+            return `❌ Unsupported block (${type === "unsupported" ? "unsupported by Notion API" : type})`;
     }
 };
-
-
-export default async function Post({params}: { params: { id: string } }) {
-    const page = await getPage(params.id);
-    const blocks = await getBlocks(params.id);
-    if (!page || !blocks) {
-        return <div/>;
-    }
-    return (
-        <div>
-            <article>
-                <h1>
-                    <Text text={page.properties.Name.title}/>
-                </h1>
-                <section>
-                    {blocks.map((block) => (
-                        <Fragment key={block.id}>{renderBlock(block)}</Fragment>
-                    ))}
-                    <Link href="/">
-                        ← Go home
-                    </Link>
-                </section>
-            </article>
-        </div>
-    );
-}
