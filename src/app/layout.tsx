8719d362dfc8596d8ba75d7aacca8b8590dbdaf8
KAN NINOMIYA <ninomiya@go5.run>
2023-08-13 03:23:17 +0900

    Header.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import Header from '@/components/Header'
 
 export const metadata: Metadata = {
   title: 'Notion on Next.js',
@@ -9,7 +10,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='ja'>
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
+import Link from 'next/link'
+
 export default async function Header() {
   return (
-    <header>
-      <h1>Notion on Next.js</h1>
+    <header className={'h-16 px-3 border-2 flex items-center'}>
+      <Link href='/'>
+        <h1>Notion on Next.js</h1>
+      </Link>
     </header>
   )
 }
