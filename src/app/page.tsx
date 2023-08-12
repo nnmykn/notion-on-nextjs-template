a06b901352ad98e68e1462267e8f72a2f976afc3
KAN NINOMIYA <ninomiya@go5.run>
2023-08-13 01:25:13 +0900

    show list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,31 @@
-export default function Home() {
-  return (
-      <div>
-        <p>hello world</p>
-      </div>
-  )
+import {getDatabase} from "@/lib/notion";
+import Link from "next/link";
+
+export default async function Home() {
+    const posts = await getDatabase();
+
+    return (
+        <div>
+            <ol>
+                {posts.map((post) => {
+                    if ('last_edited_time' in post && "properties" in post && "Name" in post.properties && "title" in post.properties.Name) {
+                        const date = new Date(post.last_edited_time).toLocaleString();
+                        const titleText = post.properties.Name.title.map(textItem => textItem.plain_text).join('');
+                        return (
+                            <li key={post.id}>
+                                <h3>
+                                    <Link href={`/${post.id}`}>
+                                        <p>{titleText}</p>
+                                    </Link>
+                                </h3>
+                                <p>{date}</p>
+                                <Link href={`/${post.id}`}>Read post →</Link>
+                            </li>
+                        );
+                    }
+                    return "データが不足しています。";
+                })}
+            </ol>
+        </div>
+    );
 }
---
+++ src/lib/notion.ts
@@ -0,0 +1,63 @@
+import {Client} from "@notionhq/client";
+import {randomInt} from 'crypto';
+
+const notion = new Client({
+    auth: process.env.NOTION_TOKEN,
+});
+
+export const getDatabase = async () => {
+    const response = await notion.databases.query({
+        database_id: process.env.NOTION_DATABASE_ID || "",
+    });
+    return response.results;
+};
+
+export const getPage = async (pageId: string) => {
+    return await notion.pages.retrieve({page_id: pageId});
+};
+
+export const getBlocks = async (blockId: string) => {
+    blockId = blockId.replaceAll("-", "");
+
+    const {results} = await notion.blocks.children.list({
+        block_id: blockId,
+        page_size: 100,
+    });
+
+    const childBlocks: any = results.map(async (block) => {
+        if (block.id) {
+            const children = await getBlocks(block.id);
+            return {...block, children};
+        }
+        return block;
+    });
+
+    return await Promise.all(childBlocks).then((blocks) => {
+        return blocks.reduce((acc, curr) => {
+            if (curr.type === "bulleted_list_item") {
+                if (acc[acc.length - 1]?.type === "bulleted_list") {
+                    acc[acc.length - 1][acc[acc.length - 1].type].children?.push(curr);
+                } else {
+                    acc.push({
+                        id: randomInt(10 ** 99, 10 ** 100).toString(),
+                        type: "bulleted_list",
+                        bulleted_list: {children: [curr]},
+                    });
+                }
+            } else if (curr.type === "numbered_list_item") {
+                if (acc[acc.length - 1]?.type === "numbered_list") {
+                    acc[acc.length - 1][acc[acc.length - 1].type].children?.push(curr);
+                } else {
+                    acc.push({
+                        id: randomInt(10 ** 99, 10 ** 100).toString(),
+                        type: "numbered_list",
+                        numbered_list: {children: [curr]},
+                    });
+                }
+            } else {
+                acc.push(curr);
+            }
+            return acc;
+        }, []);
+    });
+};
